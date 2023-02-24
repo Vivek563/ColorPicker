da4fa43fc2fa8dc534dd8f2f7bc32115313a42eb
Vivek Maurya <vivek563maurya@gmail.com>
2023-02-25 00:36:49 +0530

    js-change

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+//vivek
 const btn = document.getElementsByClassName("btn");
-const elm = document.getElementsByClassName("copy_alert");
+const cop = document.getElementsByClassName("copy_alert");
 const text = document.getElementsByClassName("text")[0];
 
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", function () {
-    const el = document.createElement("textarea");
+    const vm = document.createElement("textarea");
     let color = this.getAttribute("data-color");
     console.log("color is " + color);
-    el.value = color;
-    document.body.appendChild(el);
-    el.select();
+    vm.value = color;
+    document.body.appendChild(vm);
+    vm.select();
     document.execCommand("copy");
-    document.body.removeChild(el);
+    document.body.removeChild(vm);
 
-    elm[i].style.visibility = "visible";
+    cop[i].style.visibility = "visible";
     setTimeout(() => {
-      elm[i].style.visibility = "hidden";
+      cop[i].style.visibility = "hidden";
     }, "1000");
   });
 }
